96f8f9e2909b0e9d8bf004341ff60e74667957d8
kubaszpak <jakub.szpak.it@gmail.com>
2022-03-21 23:26:02 +0100

    changed cancel to join

--- etap1/pthreads_demo.c
+++ etap1/pthreads_demo.c
@@ -7,27 +7,35 @@
 #include <stdbool.h>
 
 #define CALCULATION_START 9999999999999999
+int *thread_lifetimes;
 
-bool is_prime(long long number) {
+bool is_prime(long long number)
+{
   assert(number >= 0);
-  if (number == 0 || number == 1) return true;
-  for (long long i = 2; i <= sqrt(number); i++) {
-      if (number % i == 0) {
-          return false;
-      }
-    }
+  if (number == 0 || number == 1)
     return true;
+  for (long long i = 2; i <= sqrt(number); i++)
+  {
+    if (number % i == 0)
+    {
+      return false;
+    }
+  }
+  return true;
 }
 
-void *perform_work(void *arguments){
+void *perform_work(void *arguments)
+{
   int index = *((int *)arguments);
   printf("THREAD %d: Started.\n", index);
-  while (true) {
-    for (long long i = CALCULATION_START;; i++) {
-      if (is_prime(i)) {
-        printf("THREAD %d found new prime number %lld\n", index, i);
-      }
+  long long i = CALCULATION_START;
+  while (thread_lifetimes[index] != -1)
+  {
+    if (is_prime(i))
+    {
+      printf("THREAD %d found new prime number %lld\n", index, i);
     }
+    i++;
   }
   printf("THREAD %d: Ended.\n", index);
   return NULL;
@@ -50,30 +58,30 @@ void *perform_work(void *arguments){
 //   return NULL;
 // }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
   assert(argc == 2);
   int number_of_threads = atoi(argv[1]);
   printf("Number of threads: %d\n", number_of_threads);
 
-  pthread_t* threads = (pthread_t*) calloc(number_of_threads, sizeof(pthread_t));
-  int* thread_args = (int*) calloc(number_of_threads, sizeof(int));
-  int* thread_lifetimes = (int*) calloc(number_of_threads, sizeof(int));
+  pthread_t *threads = (pthread_t *)calloc(number_of_threads, sizeof(pthread_t));
+  int *thread_args = (int *)calloc(number_of_threads, sizeof(int));
+  thread_lifetimes = (int *)calloc(number_of_threads, sizeof(int));
 
-  if (threads == NULL || thread_args == NULL) {
+  if (threads == NULL || thread_args == NULL)
+  {
     printf("Memory not allocated.\n");
     exit(0);
   }
 
-  int i, result_code;
-  
-  for (int i = 0; i < number_of_threads; i++) {
-    thread_lifetimes[i] = rand() % 5 + 5;
-  }
+  int result_code;
 
-  //create all threads one by one
-  for (i = 0; i < number_of_threads; i++) {
+  // create all threads one by one
+  for (int i = 0; i < number_of_threads; i++)
+  {
     printf("IN MAIN: Creating thread %d.\n", i);
     thread_args[i] = i;
+    thread_lifetimes[i] = rand() % 5 + 5;
     result_code = pthread_create(&threads[i], NULL, perform_work, &thread_args[i]);
     assert(!result_code);
   }
@@ -82,18 +90,25 @@ int main(int argc, char** argv) {
 
   int killed_threads_counter = 0;
   int seconds = 0;
-  while(killed_threads_counter < number_of_threads) {
+  while (killed_threads_counter < number_of_threads)
+  {
     sleep(1);
     seconds++;
-    for (int i = 0; i < number_of_threads; i++) {
-      if (thread_lifetimes[i] <= seconds && thread_lifetimes[i] != 0) {
+    for (int i = 0; i < number_of_threads; i++)
+    {
+      if (thread_lifetimes[i] <= seconds && thread_lifetimes[i] != -1)
+      {
         killed_threads_counter++;
-        thread_lifetimes[i] = 0;
-        result_code = pthread_cancel(threads[i]);
-        assert(!result_code);
+        thread_lifetimes[i] = -1;
         printf("IN MAIN: Thread %d has ended.\n", i);
       }
     }
+  }
+
+  for (int i = 0; i < number_of_threads; i++)
+  {
+    result_code = pthread_join(threads[i], NULL);
+    assert(!result_code);
   }
 
   free(threads);
