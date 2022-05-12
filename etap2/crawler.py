a7261becb3db540b4ebcbdb8d632b6dcc656ba69
kubaszpak <jakub.szpak.it@gmail.com>
2022-05-13 01:27:33 +0200

    Finished stage2

--- etap2/crawler.py
+++ etap2/crawler.py
@@ -8,7 +8,7 @@ SUPPORTED_COUNTRIES = {
     "United States": "us",
     "Ukraine": "ua",
     "United Kingdom": "gb",
-    "Germany": "gr",
+    "Germany": "de",
     "France": "fr"
 }
 
@@ -32,13 +32,13 @@ class App(tk.Tk):
         self.title('News reader')
         self.geometry('1280x800')
         self.resizable(0, 0)
-        self.subscriptions = []
+        self.subscriptions = {}
         self.layout()
 
     def layout(self):
-        frame_left = tk.Frame(self)
+        frame_left = ScrollableFrame(self)
         frame_left.pack(side="left", expand=True, fill="both")
-        self.frame_left = frame_left
+        self.frame_left = frame_left.scrollable_frame
         self.create_subscriptions_list()
 
     def create_subscriptions_list(self):
@@ -50,22 +50,42 @@ class App(tk.Tk):
         button_add.pack(fill="x", side="top", expand=True, pady=10, padx=10)
         self.frame_right = frame.scrollable_frame
 
-    def add_source(self, name, country_choice, category_choice):
-        sep = ttk.Separator(self.frame_right, orient="horizontal")
-        sep.pack(fill='x')
-        label_new_source = ttk.Label(
-            self.frame_right, text=name)
-        label_new_source.pack()
+    def change_thread_state(self, label_text, thread_name):
+        thread = self.subscriptions[thread_name]
+        if thread.is_thread_running():
+            print(f"Pausing subscription for: {thread_name}")
+            thread.pause()
+            label_text.set(thread_name + '\nPaused [|]')
+        else:
+            print(f"Resuming subscription for: {thread_name}")
+            thread.resume()
+            label_text.set(thread_name + '\nRunning [>]')
 
+    def add_source(self, name, country_choice, category_choice):
         kwargs = {}
         if country_choice != NONE_OPTION:
             kwargs['country_choice'] = country_choice
         if category_choice != NONE_OPTION:
             kwargs['category_choice'] = category_choice
         new_subscription = Subscription(
-            name, **kwargs)
+            name, self.frame_left, **kwargs)
+        print(f"Adding new subscription: {name} with parameters {kwargs}")
         new_subscription.start()
-        self.subscriptions.append(new_subscription)
+        self.subscriptions[name] = new_subscription
+
+        sep = ttk.Separator(self.frame_right, orient="horizontal")
+        sep.pack(fill='x')
+
+        label_text = tk.StringVar()
+        label_text.set(name + '\nRunning >>')
+        label_new_source = ttk.Label(
+            self.frame_right, textvariable=label_text)
+        label_new_source.pack()
+
+        button_change_thread_state = ttk.Button(self.frame_right,
+                                                text="Pause/Resume!",
+                                                command=lambda: self.change_thread_state(label_text, name))
+        button_change_thread_state.pack()
 
     def add_source_popup(self):
         popup = tk.Toplevel(self)
@@ -106,9 +126,12 @@ class App(tk.Tk):
             if text_name.get('1.0', 'end') == '\n':
                 print("You need to put new subscription name!")
                 return
+            elif text_name.get('1.0', 'end').strip() in self.subscriptions:
+                print("Subscription with this name already exists!")
+                return
 
             self.add_source(
-                text_name.get('1.0', 'end'), country_choice=country_choice.get(),
+                text_name.get('1.0', 'end').strip(), country_choice=country_choice.get(),
                 category_choice=category_choice.get())
             popup.destroy()
 
@@ -117,8 +140,8 @@ class App(tk.Tk):
         button_add_source.pack(fill="x", expand=True)
 
     def on_close(self):
-        for subscription in app.subscriptions:
-            print(f"Stopping subscription {subscription.name}")
+        for (subscription_name, subscription) in app.subscriptions.items():
+            print(f"Stopping subscription {subscription_name}")
             subscription.stop()
         self.destroy()
 
--- etap2/subscription.py
+++ etap2/subscription.py
@@ -1,24 +1,28 @@
 import threading
-from newsapi import NewsApiClient
 from dotenv import load_dotenv
 import os
+from tkinter import ttk
+import requests
+import webbrowser
+
+from news_api_exception import NewsAPIException
 
 load_dotenv()
 
 API_KEY = os.getenv('API_KEY')
 
-newsapi = NewsApiClient(api_key=API_KEY)
-
-THREAD_INTERVAL_IN_SECONDS = 20
+THREAD_INTERVAL_IN_SECONDS = 10
+TOP_HEADLINES_ENDPOINT = "https://newsapi.org/v2/top-headlines"
 
 
 class Subscription(threading.Thread):
-    def __init__(self, name, country_choice=None, category_choice=None):
+    def __init__(self, name, frame, country_choice=None, category_choice=None):
         super().__init__()
-
+        self.headlines = {}
         self.name = name
         self.country_choice = country_choice
         self.category_choice = category_choice
+        self.frame = frame
 
         self.__stopped = threading.Event()
         self.__running = threading.Event()
@@ -42,4 +46,41 @@ class Subscription(threading.Thread):
         self.join()
 
     def get_new_headlines(self):
-        print(f"Request for {self.name}")
+        payload = {}
+        payload['apiKey'] = API_KEY
+        if self.category_choice:
+            payload['category'] = self.category_choice
+        if self.country_choice:
+            payload['country'] = self.country_choice
+
+        print(f"Checking for new headlines on subscription: {self.name}!")
+        res = requests.get(
+            TOP_HEADLINES_ENDPOINT, timeout=10, params=payload)
+
+        if res.status_code != requests.codes.ok:
+            raise NewsAPIException(res.json())
+
+        newHeadlineFound = False
+        new_headlines = res.json()
+        for headline in new_headlines["articles"]:
+            if headline["title"] not in self.headlines:
+                newHeadlineFound = True
+                self.add_new_headline(headline)
+
+        if not newHeadlineFound:
+            print("No new headlines found!")
+
+    def add_new_headline(self, headline):
+        self.headlines[headline["title"]] = headline
+        print(f"New headline: {headline['title']}")
+        label_title = ttk.Label(
+            self.frame, text=f'{self.name}\t{headline["publishedAt"]}\t{headline["source"]["name"]}\n{headline["title"]}\t', anchor="w")
+        label_title.pack(fill="x")
+        button_open_url = ttk.Button(self.frame,
+                                     text="More...", command=lambda: webbrowser.open_new(headline["url"]))
+        button_open_url.pack()
+        sep = ttk.Separator(self.frame, orient="horizontal")
+        sep.pack(fill='x')
+
+    def is_thread_running(self):
+        return not self.__stopped.isSet()
